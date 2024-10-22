72fe83e72e91312ed2ebd5442b0d7f8e6aeab4ea
root <root@JordanPro.localdomain>
2024-10-22 11:05:55 +0200

    positive or negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
+/**
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -13,15 +16,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("is positive\n");
+		printf("is positive \n");
 	}
 	else if (n == 0)
 	{
-		printf("is zero\n");
+		printf("is zero \n");
 	}
 	else
 	{
-		printf("is negative\n");
+		printf("is negative \n");
 	}
 	return (0);
 }
