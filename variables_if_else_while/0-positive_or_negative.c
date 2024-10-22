3f9ea1936c6fe863e9d009ff5fd62c5922b0ce7d
root <root@JordanPro.localdomain>
2024-10-22 10:54:06 +0200

    positive or negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,10 +15,16 @@ int main(void)
 	printf("%d \n", n);
 
 	if (n > 0)
+	{
 		printf("is positive\n");
+	}
 	else if (n == 0)
+	{
 		printf("is zero\n");
+	}
 	else
+	{
 		printf("is negative\n");
+	}
 	return (0);
 }
