94d0fc67699fb2021ee9d84f206097a89e8d61cc
root <root@JordanPro.localdomain>
2024-10-23 11:15:51 +0200

    print with ,

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Retunr: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		if (x <= 57)
+
+	putchar(x);
+
+	putchar(',');
+
+	putchar(' ');
+
+	}
+
+		putchar('\n');
+
+		return (0);
+}
