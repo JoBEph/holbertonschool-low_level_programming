e2db6de82566907c98912522b27e93a20fe96e3a
root <root@JordanPro.localdomain>
2024-10-24 18:37:42 +0200

    print alphabet x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,22 +5,18 @@
  *
  * Return: void
  */
-
 void print_alphabet_x10(void)
 {
 	int x, y;
 
 	for (x = 0; x <= 10; x++)
 	{
-
 		y = 'a';
-	while (y <= 'z')
-	{
-		_putchar(y);
-
-		y++;
-	}
-
-	_putchar('\n');
+		while (y <= 'z')
+		{
+			_putchar(y);
+			y++;
+		}
+		_putchar('\n');
 	}
 }
