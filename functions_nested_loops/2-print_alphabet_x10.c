4bee87833e9added31bd4b4b3d3c18f02d26ee5d
root <root@JordanPro.localdomain>
2024-10-24 18:34:38 +0200

    print alphabet x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 10; x++)
+	{
+
+		y = 'a';
+	while (y <= 'z')
+	{
+		_putchar(y);
+
+		y++;
+	}
+
+	_putchar('\n');
+	}
+}
