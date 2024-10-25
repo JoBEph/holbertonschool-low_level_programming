b0f89c812a3e38d3e94e1dccdf6336487025894b
root <root@JordanPro.localdomain>
2024-10-25 14:26:13 +0200

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - print absolute value
+ *@r: int r
+ * Return: Always 0 (Success)
+ *
+ */
+
+int _abs(int r)
+{
+	printf("absolute value of int = %d\n", r);
+		return (0);
+}
