8f3ef760e344e392c4b8cdffb5367fba4516e0b0
root <root@JordanPro.localdomain>
2024-10-27 14:07:29 +0100

    add 2 values

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  add - adds two integers and returns the result to x + y
+ * @x: int
+ * @y: int
+ * Return: Always 0 (Success)
+ *
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
