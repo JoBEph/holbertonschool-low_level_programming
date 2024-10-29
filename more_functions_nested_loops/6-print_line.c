f0c7308a291daf125ff950caced96f2974e61435
root <root@JordanPro.localdomain>
2024-10-29 10:35:02 +0100

    print line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: int
+ *Return: Always 0 (Success)
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+		for (x = 0; x < n; x++)
+		{
+			_putchar('_');
+		}
+	_putchar('\n');
+}
