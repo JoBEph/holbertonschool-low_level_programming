3a4e622dcd3116959c38703dafd91bac363449f1
root <root@JordanPro.localdomain>
2024-10-30 12:17:02 +0100

    print triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -3,7 +3,7 @@
 /**
  * print_triangle - prints a triangle, followed by a new line.
  *
- * @Size - int
+ *@size: argument
  *
  * Return: Always 0 (Success)
  */
@@ -13,7 +13,7 @@ void print_triangle(int size)
 	int x, y, z;
 
 	if (size <= 0)
-	{	
+	{
 		_putchar('\n');
 	}
 	for (x = 1; x <= size; x++)
