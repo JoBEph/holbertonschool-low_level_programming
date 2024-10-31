ba8ebedfd31fa116d9474d29492f95c95a43d29f
root <root@JordanPro.localdomain>
2024-10-31 13:23:15 +0100

    swap

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -8,12 +8,11 @@
  * Return: Always 0 (Success)
  */
 
-void swap_int(int *a, int *b);
+void swap_int(int *a, int *b)
 {
-int temp;
+	int temp;
 
-temp = a;
-a = b
-b = temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
-return (0);
