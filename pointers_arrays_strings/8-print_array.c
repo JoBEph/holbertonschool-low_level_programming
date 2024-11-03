3c1dbede1aa0b5d2c486cce77a7903fd4fb2594c
root <root@JordanPro.localdomain>
2024-11-03 15:01:04 +0100

    print array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,14 +2,16 @@
 
 /**
  * print_array - function that prints n elements of an array of integers
- * a: pointer
- * n: int
+ * @a: pointer
+ * @n: int
  * Return: Always 0 (Success)
  */
 void print_array(int *a, int n)
 {
 	int x = 0;
-	for(; x < n; x++)
+
+	for (; x < n; x++)
+
 	{
 		printf("%d", a[x]);
 		if (x < n - 1)
