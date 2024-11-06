58214f49288776447ee68294ae57a61caa12f088
root <root@JordanPro.localdomain>
2024-11-06 10:42:03 +0100

    print rev recursion

---
+++ recursion/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <ctype.h>
+
+/* prototype */
+
+int _putchar(char c);
+
+void _puts_recursion(char *s);
+
+void _puts(char *str);
+
+void _print_rev_recursion(char *s);
+
+#endif
