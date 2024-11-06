e81a46e218cd36b2a446e160654b684565964fc3
root <root@JordanPro.localdomain>
2024-11-06 21:30:28 +0100

    whats my name

---
+++ argc_argv/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <ctype.h>
+
+/* prototype */
+
+int _putchar(char c);
+
+void _puts_recursion(char *s);
+
+void _puts(char *str);
+
+#endif
