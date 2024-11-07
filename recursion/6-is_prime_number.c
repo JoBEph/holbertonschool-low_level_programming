266b234773aff71cbacf831429158153232daaef
root <root@JordanPro.localdomain>
2024-11-07 11:42:04 +0100

    prime number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -13,26 +13,23 @@ int is_number(int n, int x)
 	{
 		return (0);
 	}
-	for (x = 2; x * x <= n; x++)
+	if (x * x > n)
+		return (1);
 	{
 		if (n % x == 0)
 		{
 			return (0);
 		}
 	}
-	return (1);
+	return (is_number(n, x + 1));
 }
 /**
- * is_prime_number - print number
- * @n: int
- * Return: Always 0 (Success)
- */
+* is_prime_number - print number
+* @n: int
+* Return: Always 0 (Success)
+*/
 
 int is_prime_number(int n)
 {
-	if (n == 0)
-	{
-		return (1);
-	}
-	return (is_number(n, 0));
+return (is_number(n, 2));
 }
