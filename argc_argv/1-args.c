426d08e1491e8269d06acbe2c0c2c931dff9e7fe
root <root@JordanPro.localdomain>
2024-11-07 17:24:54 +0100

    args

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -2,20 +2,22 @@
 
 /**
  * main - prints the number of arguments passed into it
- *
- *
- *
+ * @argc: - int
+ * @argv: - pointer
+ * Return: Always 0 ( Success)
  */
 int main(int argc, char *argv[])
 {
 	int x;
 
-	printf("%d",argc);
-	
-	for(x = 1; x <argc; x++)
+	printf("%d", argc);
+
+	for (x = 1; x < argc; x++)
 	{
-		printf("%s",argv[x]);
+		printf("%d%s", x, argv[x]);
 	}
+
 	printf("\n");
-    	return 0;
+
+	return (0);
 }
