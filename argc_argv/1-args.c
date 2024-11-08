992cf1b1f06072b7d2559711023f434295369e92
root <root@JordanPro.localdomain>
2024-11-08 09:08:46 +0100

    args

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -8,16 +8,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int x;
+	(void)argv;
 
-	printf("%d", argc);
-
-	for (x = 1; x < argc; x++)
-	{
-		printf("%d%s", x, argv[x]);
-	}
-
-	printf("\n");
-
-	return (0);
+		printf("%d\n", argc);
+		return (0);
 }
