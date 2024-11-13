489ff1d6b77f8d375dd8658ab4657b14d2b093d2
root <root@JordanPro.localdomain>
2024-11-13 11:49:13 +0100

    free grid

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * free_grid - function that frees a 2 dimensional
+ *@grid: double pointer
+ *@height: int
+ * Return: Always 0 (Success)
+ */
+
+void free_grid(int **grid, int height)
+{
+	int x;
+
+	for (x = 0; x < height; x++)
+	{
+		free(grid[x]);
+	}
+	
+	free(grid);
+}
