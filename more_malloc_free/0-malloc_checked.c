f6ff9ad689908f23fa19aa3d641458b39bce8d81
root <root@JordanPro.localdomain>
2024-11-13 13:36:58 +0100

    malloc check

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -11,7 +11,10 @@ void *malloc_checked(unsigned int b)
 	void *x;
 
 	x = malloc(b);
+
 	if (x == NULL)
-	exit(98);
+	{
+		exit(98);
+	}
 	return (x);
 }
---
+++ more_malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/* prototypes */
+
+void *malloc_checked(unsigned int b);
+
+#endif
