bb63979011d78105bf3df20ce34270058fddb87f
root <root@JordanPro.localdomain>
2024-11-15 10:57:41 +0100

    alloc grid

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -13,27 +13,28 @@ int **alloc_grid(int width, int height)
 	int **grid;
 
 	if (width <= 0 || height <= 0)
-	{
 		return (NULL);
-	}
+
 	grid = malloc(sizeof(int *) * height);
 
 	if (grid == NULL)
-	{
 		return (NULL);
-	}
+
 	for (x = 0; x < height; x++)
 	{
-		grid[x] = malloc(sizeof(int) * width);
+		grid[x] = (int *)malloc(sizeof(int) * width);
+
 		if (grid[x] == NULL)
 		{
 			for (y = 0; y < x; x++)
-		{
 				free(grid[y]);
-		}
+
 			free(grid);
 			return (NULL);
 		}
+	}
+	for (x = 0; x < height; x++)
+	{
 		for (y = 0; y < width; y++)
 		{
 			grid[x][y] = 0;
