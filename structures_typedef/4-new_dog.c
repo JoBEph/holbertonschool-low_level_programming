bfe499c9352c174eaa5307de6a973e3f85ed4418
root <root@JordanPro.localdomain>
2024-11-16 14:44:37 +0100

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,8 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
 /**
  * new_dog - function that creates a new dog
  * @name: char
@@ -13,28 +10,44 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-	int x, y;
+	int x, y, z;
+
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-	x = strlen(name);
-	d->name = malloc(sizeof(char) * (x +1));
-	if (d->name == NULL)
+	
+	if (name != NULL)
 	{
-		free(d);
-		return(NULL);
+		for (x = 0; name[x] != '\0'; x++)
+
+		d->name = malloc(sizeof(char) * x);
+		if (d->name == NULL)
+		{
+			free(d);
+			return(NULL);
+		}
+		for (z = 0; z < x; z++)
+			d->name[z] = name[z];
 	}
-	y = strlen(owner);
-	d->owner = malloc(sizeof(char) * (y + 1));
+	else
+		d->name = NULL;
+	d->age = age;
+	if (owner != NULL)
+	{
+		for (y = 0; owner[y] != '\0'; y++)
+
+	d->owner = malloc(sizeof(char) * y);
 	if (d->owner == NULL)
 	{
 		free(d->name);
 		free(d);
 		return (NULL);
 	}
-	strcpy(d->name, name);
-	strcpy(d->owner, owner);
-	d->age = age;
+	for (z = 0; z < y; z++)
+		d->owner[z] = owner[z];
+	}
+	else
+		d->owner = NULL;
 
 	return (d);
 }
