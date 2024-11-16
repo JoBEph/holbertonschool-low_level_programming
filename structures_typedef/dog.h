5c95fe0d5d3bbc0147d6ea80c1c75a61eb624365
root <root@JordanPro.localdomain>
2024-11-16 12:44:27 +0100

    info dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct my_struct - Short description
+ *@name: char
+ *@age: float
+ *@owner: char
+ *
+ * Description: Define a new type struct dog with the following elements
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
