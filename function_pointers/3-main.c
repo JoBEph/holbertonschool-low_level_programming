b87f4c82622b2e9dd6e2defc20af6605aa21b225
root <root@JordanPro.localdomain>
2024-11-21 12:17:36 +0100

    calcoulatrice

---
+++ function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+
+/**
+ *main - mainc
+ *@argc: argc
+ *@argv: argv
+ *Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	op = get_op_func(argv[2]);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if (argv[2] == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if (b == 0 && (argv[2][0] == 37 || argv[2][0] == 47))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+		printf("%d\n", op(a, b));
+
+		return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+/**
+ *op_add - addition
+ *@a: int
+ *@b: int
+ *Return: 0
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *op_sub - soustraction
+ *@a: int
+ *@b: int
+ *Return: 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply
+ * @a: int
+ * @b: int
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division
+ * @a: int
+ * @b: int
+ * Return: Always 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulo
+ * @a: int
+ * @b: int
+ * Return: Always 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
