640cf26506f5622f94b8130c8496bd02970b3784
root <root@JordanPro.localdomain>
2024-11-21 05:33:06 +0100

    print numbers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers
+ * @separator: const char
+ * @n: const int
+ * Return: Always 0
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int x;
+	int numbers = 0;
+	va_list xlist;
+
+	va_start(xlist, n);
+
+	for (x = 0; x < n; x++)
+	{
+		numbers = (int) va_arg(xlist, int);
+		printf("%d", numbers);
+
+		if (separator != NULL)
+			printf("%s", separator);
+	}
+	va_end(xlist);
+
+	printf("\n");
+}
