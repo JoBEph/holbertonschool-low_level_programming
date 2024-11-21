05c6654d1ad09d0fe5b88f1ea64a259e98477f05
root <root@JordanPro.localdomain>
2024-11-21 06:03:57 +0100

    print string

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function print string
+ * @separator: pointer char
+ * @n: const uns int
+ * Return: Always 0
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int x;
+	const char *strings;
+	va_list xlist;
+
+	va_start(xlist, n);
+
+	for (x = 0; x < n; x++)
+	{
+		strings = va_arg(xlist, const char *);
+			printf("%s", strings);
+
+		if (separator != NULL && x != (n - 1))
+			printf("%s", separator);
+	}
+	va_end(xlist);
+	printf("\n");
+}
